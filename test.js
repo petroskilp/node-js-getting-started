bcc7d026aaa5f9ec0e9e4cb05fdfc58486a15fad
Luiz Pedro Petroski <petroskilp@gmail.com>
2019-08-19 18:01:55 -0300

    Atualizando

--- test.js
+++ test.js
@@ -7,7 +7,7 @@ const env = Object.assign({}, process.env, {PORT: 5000});
 const child = spawn('node', ['index.js'], {env});
 
 test('responds to requests', (t) => {
-  t.plan(4);
+  t.plan(12);
 
   // Wait until the server is ready
   child.stdout.on('data', _ => {
