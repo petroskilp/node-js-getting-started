7d2f4199dd3db4702f1c1910298388e15da784dd
Luiz Pedro Petroski <petroskilp@gmail.com>
2019-09-01 16:42:04 -0300

    Atualizando

--- tests/test.js
+++ tests/test.js
@@ -5,7 +5,7 @@ const app = require('../index');
 chai.use(chaiHttp);
 chai.should();
 describe("Teste de request para contatos", () => {
-    describe("GET /contatos", () => {
+    describe("Rotas para /contatos", () => {
         // Testar a requisição para listagem de todos contatos
         it("deve listar todos contatos", (done) => {
             chai.request(app)
@@ -20,15 +20,7 @@ describe("Teste de request para contatos", () => {
         // Testar a requisição da tela de edição de contato
         it("deve mostrar a tela de edição de contatos", (done) => {
             // Criar um novo contato par realizar a edição do mesmo
-            var contato = new app.models.contatos({
-                nome: 'Fulano',
-                sobrenome: 'de Tal',
-                email: 'fulano@fulano.com',
-                cidade: 'Ponta Grossa',
-                pais: 'Brasil',
-                profissao: 'Programador'
-            });
-            contato.save();
+           
             chai.request(app)
                 .get(`/contatos/${contato._id}/editar`)
                 .end((err, res) => {
